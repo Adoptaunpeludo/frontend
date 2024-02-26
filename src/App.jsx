a262c04e0f2985975611515052f1baf94400f5ce
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-27 00:53:35 +0100

    feat: initial router setup with landing, login, register and error pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,57 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  AppLayout,
+  ErrorPage,
+  LandingPage,
+  LoginPage,
+  RegisterPage,
+} from './pages';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 60 * 15,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: 'register',
+        element: <RegisterPage />,
+        // action:
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+        // action:
+      },
+      // {
+      //   path: "dasboardh"
+      //   ...
+      // }
+    ],
+  },
+]);
+
 function App() {
-  return <>App</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
