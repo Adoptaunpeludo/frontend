c5e2d35ec9b9f52a63fc3d97fa97ade5e732888d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-02-27 01:02:38 +0100

    feat: Landing, Register, Login basic structure

--- src/pages/Auth/Login/LoginPage.jsx
+++ src/pages/Auth/Login/LoginPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="container">
+      <h1>Login Page</h1>
+      <br />
+      <Link to="/register">Register</Link>
+      <br />
+      <br />
+      <Link to="/">Landing</Link>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="container">
+      <h1>Landing Page</h1>
+      <br />
+      <Link to="/register">Register</Link>
+      <br />
+      <br />
+      <Link to="/login">Login</Link>
+    </div>
+  );
 };
 
 export default LandingPage;
