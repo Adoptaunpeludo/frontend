e59f4b649506af692a7acc5f14ce9e6e7c5661c1
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-02-28 00:17:21 +0100

    FIX: remove innecesary usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import {
   AppLayout,
@@ -47,8 +46,6 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
