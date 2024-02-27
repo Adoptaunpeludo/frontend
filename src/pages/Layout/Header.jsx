75a32b99a964430219a97609ada5b907063a042b
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-02-28 00:07:29 +0100

    Feat: Add header component

---
+++ src/pages/Layout/Header.jsx
@@ -0,0 +1,77 @@
+import {
+  Button,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle
+} from '@nextui-org/react';
+import React from 'react';
+
+import LoginIcon from '../../assets/icons/LoginIcon.jsx';
+import BrandNavLogo from '../../assets/logos/BrandNavLogo.jsx';
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = ['Home', 'Perros', 'Gatos', 'Asociaciones'];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth='xl'>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className='sm:hidden'
+        />
+        <NavbarBrand>
+          <BrandNavLogo />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className='hidden sm:flex gap-4' justify='center'>
+        <NavbarItem>
+          <Link color='foreground' href='#'>
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color='foreground' href='#'>
+            Perros
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color='foreground' href='#'>
+            Gatos
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
+        <NavbarItem>
+          <Button
+            as={Link}
+            color='primary'
+            href='#'
+            variant='solid'
+            size='sm'
+            endContent={<LoginIcon />}
+          >
+            Login
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link className='w-full' href='#' size='lg' color='foreground'>
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+};
+export default Header;
