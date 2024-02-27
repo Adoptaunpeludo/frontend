b975786a011c6fbd43afe54fe050dae1191c296c
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-02-28 00:16:38 +0100

    Feat: apply header and footer component

--- src/pages/Layout/AppLayout.jsx
+++ src/pages/Layout/AppLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
+import Header from './Header';
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className='h-dvh'>
+      <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 };
