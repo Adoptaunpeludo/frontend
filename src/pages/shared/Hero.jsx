7db83a88a86d1e58bad0265d51da6d675c5df4d0
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-02-28 19:29:59 +0100

    Feat: hero component

---
+++ src/pages/shared/Hero.jsx
@@ -0,0 +1,10 @@
+// TODO:prop from object array with all hero images
+const Hero = () => {
+  return (
+    <section
+      id='hero'
+      className="w-full bg-[url('/hero/login_hero.jpg')] h-96 bg-bottom bg-no-repeat "
+    ></section>
+  );
+};
+export default Hero;
