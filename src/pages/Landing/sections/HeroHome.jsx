8d6d4418a2459f252b9ba6c9a8bd624c56846655
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-02-29 01:16:18 +0100

    Feat: hero Home

--- src/pages/Landing/sections/HeroHome.jsx
+++ src/pages/Landing/sections/HeroHome.jsx
@@ -1,10 +1,41 @@
+import { Button } from '@nextui-org/react';
+import Imago from '../../../assets/logos/imago.svg?react';
 const HeroHome = () => {
   return (
     <section
       id='hero-home'
-      className='bg-red-100 h-60 flex justify-center items-center'
+      className="w-full bg-[url('/hero/home-hero.png')] h-screen bg-top bg-cover bg-no-repeat"
     >
-      <h1>HERO HOME</h1>
+      <main className='max-w-screen-xl w-full flex flex-col gap-3 h-full justify-start  mx-auto px-5'>
+        <div
+          id='hero-content'
+          className='flex flex-col max-w-lg h-max my-auto gap-1'
+        >
+          <div id='hero-image' className='flex flex-col mx-auto'>
+            <Imago />
+          </div>
+          <div id='hero-text' className='flex flex-col justify-start '>
+            <div className='font-poppins text-xs'>
+              NON-PROFIT ANIMAL SHELTER
+            </div>
+            <div className='font-lobster text-7xl text-secondary'>
+              Adopta un peludo
+            </div>
+            <div className='my-4 text-pretty max-w-md'>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur
+              </p>
+            </div>
+          </div>
+          <div id='hero-CTA' className='flex flex-col justify-center mx-auto'>
+            <Button size='md' color='primary' className='w-32'>
+              <span className='font-lobster text-xl text-white'>Adopta</span>
+            </Button>
+          </div>
+        </div>
+      </main>
     </section>
   );
 };
