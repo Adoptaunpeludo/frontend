a896959808fe88a3deb5c37cf77a0465cb1a0497
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-01 21:28:16 +0100

    Feat: add Joinsletter

--- src/pages/Landing/sections/JoinNewsletter.jsx
+++ src/pages/Landing/sections/JoinNewsletter.jsx
@@ -1,10 +1,50 @@
+import { Button, Input } from '@nextui-org/react';
+import { IconMailFilled } from '@tabler/icons-react';
 const JoinNewsletter = () => {
   return (
     <section
       id='join-newsletter'
-      className='bg-red-800 h-60 flex justify-center items-center'
+      className="w-full bg-[url('/public/backgrounds/join-newsletter.jpg')] h-max bg-bottom bg-cover bg-no-repeat py-40"
     >
-      <h1>Join Newsletter</h1>
+      <main
+        id='newsletter-background'
+        className='max-w-screen-xl w-full flex flex-col h-96  background-panel-newsletter mx-auto'
+      >
+        <form
+          action=''
+          id='form-newsletter'
+          className='flex flex-col max-w-9xl h-max my-auto gap-8 items-center '
+        >
+          <div
+            id='newsletter title'
+            className='text-primary font-lobster text-7xl '
+          >
+            Únete a nuestra newsletter
+          </div>
+          <div id='newsletter input' className='flex flex-col justify-start '>
+            <Input
+              className='min-w-72 '
+              type='email'
+              label='Email'
+              placeholder='Introduce tu email'
+            ></Input>
+          </div>
+          <div
+            id='newsletter button'
+            className='flex flex-col justify-center mx-auto'
+          >
+            <Button
+              color='primary'
+              href='#'
+              variant='solid'
+              size='lg'
+              endContent={<IconMailFilled />}
+            >
+              Suscríbete
+            </Button>
+          </div>
+        </form>
+      </main>
     </section>
   );
 };
