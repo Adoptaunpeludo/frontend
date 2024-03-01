c7f0e6428de17a41cce303026eb0f2f803f039ae
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-01 21:27:10 +0100

    Feat: Add featured cats

--- src/pages/Landing/sections/FeaturedCats.jsx
+++ src/pages/Landing/sections/FeaturedCats.jsx
@@ -2,9 +2,18 @@ const FeaturedCats = () => {
   return (
     <section
       id='featured-cats'
-      className='bg-red-500 h-60 flex justify-center items-center'
+      className="w-full  max-h-screen  bg-left-bottom bg-auto bg-no-repeat h-screen bg-black bg-[url('/backgrounds/featured-cats.jpg')] relative"
     >
-      <h1>Featured Cats</h1>
+      <main
+        id='featured-cats-content'
+        className='max-w-screen-xl w-full flex flex-col gap-3 justify-start  mx-auto px-5 h-96 '
+      ></main>
+      <div id='vector' className='absolute bottom-0 w-full'>
+        <img
+          src='/public/backgrounds/featured-cats-vector.png'
+          className='w-screen'
+        ></img>
+      </div>
     </section>
   );
 };
