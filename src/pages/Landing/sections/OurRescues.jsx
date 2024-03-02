70bd6429a12a8bc1f999b0c525f3db267d682cee
Doplax <doplax@gmail.com>
2024-03-02 20:58:21 +0100

    fix(landing): add unique key props to list items in FeaturedShelters and OurRescues

--- src/pages/Landing/sections/OurRescues.jsx
+++ src/pages/Landing/sections/OurRescues.jsx
@@ -26,8 +26,9 @@ const OurRescues = () => {
             id='goals'
             className='my-4 max-w-screen-xl w-full flex max-lg:flex-col  justify-evenly px-12 py-12 gap-5'
           >
-            {rescues.map(rescue => (
+            {rescues.map((rescue, key) => (
               <RescuesIcons
+                key={key}
                 image={rescue.image}
                 number={rescue.number}
                 label={rescue.label}
