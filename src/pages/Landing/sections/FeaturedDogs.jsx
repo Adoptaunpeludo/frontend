5eca8ee0759e9c4d9c9583776edd8573a05de484
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-02 20:23:24 +0100

    Feat Featured dogs section

--- src/pages/Landing/sections/FeaturedDogs.jsx
+++ src/pages/Landing/sections/FeaturedDogs.jsx
@@ -1,18 +1,27 @@
+import Panel from '../../shared/Panel';
+import FeaturedPets from '../components/FeaturedPets';
+const pets = [
+  { image: 'perrete-01.jpg', name: 'Max', age: 'Cachorro', breed: 'Lebrel' },
+  { image: 'perrete-01.jpg', name: 'Max', age: 'Cachorro', breed: 'Lebrel' },
+  { image: 'perrete-01.jpg', name: 'Max', age: 'Cachorro', breed: 'Lebrel' },
+  { image: 'perrete-01.jpg', name: 'Max', age: 'Cachorro', breed: 'Lebrel' }
+];
 const FeaturedDogs = () => {
   return (
     <section
       id='featured-dogs'
-      className="w-full  max-h-screen  bg-right-bottom bg-auto bg-no-repeat h-screen bg-white bg-[url('/backgrounds/featured-dogs.jpg')] relative"
+      className="w-full bg-right-bottom bg-auto bg-no-repeat h-max bg-white bg-[url('/backgrounds/featured-dogs.jpg')] relative flex justify-center"
     >
       <main
         id='featured-dogs-content'
-        className='max-w-screen-xl w-full flex flex-col gap-3 justify-start  mx-auto px-5 h-96 '
-      ></main>
+        className='max-w-screen-xl w-full flex gap-3 justify-start max-sm:justify-center  px-5  h-max '
+      >
+        <Panel className='max-w-2xl px-4 py-4 mb-52 '>
+          <FeaturedPets title={'Perretes'} pets={pets}></FeaturedPets>
+        </Panel>
+      </main>
       <div id='vector' className='absolute bottom-0 w-full'>
-        <img
-          src='/backgrounds/featured-dogs-vector.png'
-          className='w-screen'
-        ></img>
+        <img src='/backgrounds/featured-dogs-vector.png' className='w-screen' />
       </div>
     </section>
   );
