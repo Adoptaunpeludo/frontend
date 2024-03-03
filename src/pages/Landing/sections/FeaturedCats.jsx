4540fa2652b98bc755c8a2692a9d9645722909eb
Juan Manuel Acosta Benítez <jmab2k@gmail.com>
2024-03-03 12:49:27 +0100

    Revert "New pull  Request with the changes no applied"

--- src/pages/Landing/sections/FeaturedCats.jsx
+++ src/pages/Landing/sections/FeaturedCats.jsx
@@ -1,45 +1,13 @@
-import Panel from '../../shared/Panel';
-import FeaturedPets from '../components/FeaturedPets';
-const pets = [
-  {
-    image: 'gatete-01.jpg',
-    name: 'Silvester',
-    age: 'Cachorro',
-    breed: 'Scottish Fold'
-  },
-  {
-    image: 'gatete-01.jpg',
-    name: 'Silvester',
-    age: 'Cachorro',
-    breed: 'Scottish Fold'
-  },
-  {
-    image: 'gatete-01.jpg',
-    name: 'Silvester',
-    age: 'Cachorro',
-    breed: 'Scottish Fold'
-  },
-  {
-    image: 'gatete-01.jpg',
-    name: 'Silvester',
-    age: 'Cachorro',
-    breed: 'Scottish Fold'
-  }
-];
 const FeaturedCats = () => {
   return (
     <section
       id='featured-cats'
-      className="w-full   bg-left-bottom bg-auto bg-no-repeat h-max bg-black bg-[url('/backgrounds/featured-cats.jpg')] relative flex justify-center"
+      className="w-full  max-h-screen  bg-left-bottom bg-auto bg-no-repeat h-screen bg-black bg-[url('/backgrounds/featured-cats.jpg')] relative"
     >
       <main
         id='featured-cats-content'
-        className='max-w-screen-xl w-full flex gap-3 justify-end max-sm:justify-center  px-5  h-max '
-      >
-        <Panel className='max-w-2xl px-4 py-4 mb-52 background-panel-cats'>
-          <FeaturedPets title={'Gatetes'} pets={pets}></FeaturedPets>
-        </Panel>
-      </main>
+        className='max-w-screen-xl w-full flex flex-col gap-3 justify-start  mx-auto px-5 h-96 '
+      ></main>
       <div id='vector' className='absolute bottom-0 w-full'>
         <img src='/backgrounds/featured-cats-vector.png' className='w-screen' />
       </div>
--- src/pages/Landing/sections/OurRescues.jsx
+++ src/pages/Landing/sections/OurRescues.jsx
@@ -26,11 +26,9 @@ const OurRescues = () => {
             id='goals'
             className='my-4 max-w-screen-xl w-full flex max-lg:flex-col  justify-evenly px-12 py-12 gap-5'
           >
-
-            {rescues.map((rescue, index) => (
+            {rescues.map((rescue, key) => (
               <RescuesIcons
-                key={index}
-
+                key={key}
                 image={rescue.image}
                 number={rescue.number}
                 label={rescue.label}
