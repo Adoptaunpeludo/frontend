b89b6b2906f231ae908865859e4c999a16fc2c3d
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-04 21:37:30 +0100

    Fix: change background features cats

--- src/pages/Landing/sections/FeaturedCats.jsx
+++ src/pages/Landing/sections/FeaturedCats.jsx
@@ -35,7 +35,7 @@ const FeaturedCats = () => {
     >
       <main
         id='featured-cats-content'
-        className="max-w-screen-xl w-full flex gap-3 justify-end max-sm:justify-center  px-5  h-max  bg-left-bottom bg-auto bg-no-repeat bg-[url('/backgrounds/featured-cats-2.jpg')]"
+        className="max-w-screen-xl w-full flex gap-3 justify-end max-sm:justify-center  px-5  h-max  bg-left-bottom bg-auto bg-no-repeat bg-[url('/backgrounds/featured-cats.jpg')]"
       >
         <Panel className='max-w-2xl px-4 py-4 mb-80 background-panel-cats'>
           <FeaturedPets title={'Gatetes'} pets={pets}></FeaturedPets>
