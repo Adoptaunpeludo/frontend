427bc27dec21fcc9dfb87238186ea0eec136249e
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-05 23:13:08 +0100

    Feat: add pages to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-export { default as AppLayout } from './Layout/AppLayout';
-export { default as ErrorPage } from './Error/ErrorPage';
-export { default as LandingPage } from './Landing/LandingPage';
-export { default as RegisterPage } from './Auth/Register/RegisterPage';
+export { default as AnimalDetailsPage } from './AnimalDetails/AnimalDetailsPage';
 export { default as LoginPage } from './Auth/Login/LoginPage';
+export { default as RegisterPage } from './Auth/Register/RegisterPage';
 export { default as CatsPage } from './Cats/CatsPage';
 export { default as DogsPage } from './Dogs/DogsPage';
+export { default as ErrorPage } from './Error/ErrorPage';
+export { default as LandingPage } from './Landing/LandingPage';
+export { default as AppLayout } from './Layout/AppLayout';
+export { default as SheltersProfile } from './Private/SheltersProfile';
 export { default as SheltersPage } from './Shelters/SheltersPage';
-export { default as AnimalDetailsPage } from './AnimalDetails/AnimalDetailsPage';
-
