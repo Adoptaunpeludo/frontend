cd9aa9302c64bb840e5739f5c24345ee94a40479
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-06 22:59:02 +0100

    Feat: component for fields aside

---
+++ src/pages/shared/ProfileAsideField.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+export function ProfileAsideField({ fieldName, fieldValue }) {
+  return (
+    <div className='flex justify-between border-solid border-b-1 border-b-primary mx-2'>
+      <span>{fieldName}</span>
+      <span>{fieldValue}</span>
+    </div>
+  );
+}
+
+ProfileAsideField.propTypes = {
+  fieldName: PropTypes.string.isRequired,
+  fieldValue: PropTypes.string
+};
