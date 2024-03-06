f56fabc0918b70f8f4cfa2581e445916dce7faed
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-06 22:59:20 +0100

    add components to shared file

--- src/pages/shared/index.js
+++ src/pages/shared/index.js
@@ -1,8 +1,12 @@
 export { FilterBar } from './FilterBar';
+export { H2Title } from './H2Title';
+export { H3Title } from './H3Title';
+export { H4Title } from './H4Title';
 export { Hero } from './Hero';
 export { LogoHeader } from './LogoHeader';
 export { Panel } from './Panel';
 export { PetCard } from './PetCard';
+export { ProfileAsideField } from './ProfileAsideField';
 export { TitleSection } from './TitleSection';
 
 //! TODO: Mover todos estos componentes a una carpeta Components/Shared
