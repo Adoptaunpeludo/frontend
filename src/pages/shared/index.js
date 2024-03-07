1726ce68d22a350dbab1119a063e5d48deb47bf7
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-07 23:09:39 +0100

    Feat: add new imports components

--- src/pages/shared/index.js
+++ src/pages/shared/index.js
@@ -1,3 +1,4 @@
+export { EditButton } from './EditButton';
 export { FilterBar } from './FilterBar';
 export { H2Title } from './H2Title';
 export { H3Title } from './H3Title';
@@ -8,5 +9,6 @@ export { Panel } from './Panel';
 export { PetCard } from './PetCard';
 export { ProfileAsideField } from './ProfileAsideField';
 export { TitleSection } from './TitleSection';
+export { UserInformation } from './UserInformation';
 
 //! TODO: Mover todos estos componentes a una carpeta Components/Shared
