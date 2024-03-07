051d8183de60a6985a7b4b9c819612af659354e2
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-07 19:25:18 +0100

    Feat: add useShelter function with mocked data

---
+++ src/pages/Private/ShelterProfile/useShelterProfile.jsx
@@ -0,0 +1,27 @@
+export const useShelterProfile = () => {
+  const data = {
+    cif: 'X.99.999.999',
+    legalForms: 'Asociación',
+    ownVet: true,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultricies dapibus leo, sed tempus dolor commodo vel. Cras non metus in ex condimentum congue id eu lacus. Etiam eleifend interdum mollis. Nunc ante erat, laoreet nec ante gravida, mattis euismod turpis. Aliquam dictum egestas lectus vitae rutrum. Curabitur ornare faucibus sapien nec semper. Nam eget fringilla velit.',
+    id: '052f1404-ff7a-4ec1-9919-64656f5577cb',
+    email: 'jmab2k@gmail.com',
+    username: 'jmab2k',
+    createdAt: '2024-03-07T17:55:43.302Z',
+    avatar: 'avatar.png',
+    role: 'shelter',
+    dni: null,
+    firstName: 'John',
+    isOnline: false,
+    lastName: 'Doe',
+    veterinaryFacilities: true,
+    facilities: [],
+    images: [],
+    socialMedia: []
+  };
+  const isLoading = false;
+  const isError = false;
+
+  return { data, isLoading, isError };
+};
