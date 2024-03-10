a02185af0604c92ee5d0c29e15009371167cd97c
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-10 21:39:03 +0100

    Feat: add UserFormBio

--- src/pages/Private/index.js
+++ src/pages/Private/index.js
@@ -1,2 +1,3 @@
 export { default as AdopterProfile } from './AdopterProfile/AdopterProfile';
 export { default as ShelterProfile } from './ShelterProfile/ShelterProfile';
+export { default as UserFormBio } from './UserFormBio/UserFormBio.jsx';
