1cdaf693fa548131e19c56920534575c237e1919
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2024-03-12 17:24:17 +0100

    feat: cats and dogs showing correctly along with images in public zones

--- src/pages/Landing/components/FeaturedPets.jsx
+++ src/pages/Landing/components/FeaturedPets.jsx
@@ -1,25 +1,33 @@
-import { Button } from '@nextui-org/react';
+import { Button, Link } from '@nextui-org/react';
 import PetCardLanding from '../components/PetCardLanding.jsx';
 const FeaturedPets = ({ title, pets }) => {
   return (
-    <section className='flex flex-col gap-4 h-max z-30  '>
+    <section className="flex flex-col gap-4 h-max z-30  ">
       <header>
-        <h2 className='font-lobster text-5xl text-secondary'>{title}</h2>
+        <h2 className="font-lobster text-5xl text-secondary">{title}</h2>
       </header>
-      <main className='flex flex-col gap-2'>
-        {pets.map(({ image, name, age, breed }, index) => (
+      <main className="flex flex-col gap-2">
+        {pets.map(({ images, name, age, breed }, index) => (
           <PetCardLanding
             key={index}
-            image={image}
+            image={`${import.meta.env.VITE_ANIMALS_IMAGES_BASE_URL}/${
+              images[0]
+            }`}
             name={name}
             age={age}
             breed={breed}
           />
         ))}
       </main>
-      <footer className='mx-auto'>
-        <Button size='md' color='primary' className='w-32'>
-          <span className='font-lobster text-xl text-white'>Ver todos</span>
+      <footer className="mx-auto">
+        <Button
+          as={Link}
+          href={title === 'Gatetes' ? '/cats' : '/dogs'}
+          size="md"
+          color="primary"
+          className="w-32"
+        >
+          <span className="font-lobster text-xl text-white">Ver todos</span>
         </Button>
       </footer>
     </section>
