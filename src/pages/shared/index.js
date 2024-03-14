f635f2f599382d3fa2d162305437475ce0909f8f
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-14 21:28:34 +0100

    Feat: add component to file

--- src/pages/shared/index.js
+++ src/pages/shared/index.js
@@ -9,6 +9,7 @@ export { Hero } from './Hero';
 export { LogoHeader } from './LogoHeader';
 export { Panel } from './Panel';
 export { PetCard } from './PetCard';
+export { SelectField } from './SelectField';
 export { TitleSection } from './TitleSection';
 
 //! TODO: Mover todos estos componentes a una carpeta Components/Shared
