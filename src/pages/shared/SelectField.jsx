02f2e662025303aa7a8452a10573937a1adef20b
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-14 21:29:12 +0100

    FEAT: Custom select component with specific fields

---
+++ src/pages/shared/SelectField.jsx
@@ -0,0 +1,21 @@
+import { Select, SelectItem } from '@nextui-org/react';
+
+export const SelectField = ({ label, dataField, dataEnum, className }) => {
+  return (
+    <Select
+      className={className}
+      label={label}
+      name={dataField}
+      placeholder={
+        dataField === '' || dataField === null ? 'Seleccionar...' : ''
+      }
+      defaultSelectedKeys={dataField !== '' && [dataField]}
+    >
+      {dataEnum.map(({ label, value }) => (
+        <SelectItem key={value} value={value}>
+          {label}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+};
