c7e2f433443254a594bd6e3e73577b510d1e904a
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-15 22:37:26 +0100

    FIX: Change const URL for animal Image

--- src/pages/AnimalDetails/AnimalDetailsPage.jsx
+++ src/pages/AnimalDetails/AnimalDetailsPage.jsx
@@ -1,20 +1,20 @@
-import { Spinner, Image, Button } from '@nextui-org/react';
-import ErrorPage from '../Error/ErrorPage';
-import { useAnimalDetails, animalDetailsQuery } from './useAnimalDetails';
-import { TitleSection } from '../../components';
-import { HeartIcon } from '../../assets/svg';
-import { MinimalLogo } from '../../assets/logos';
+import { Button, Image, Spinner } from '@nextui-org/react';
 import { useLoaderData } from 'react-router-dom';
+import { MinimalLogo } from '../../assets/logos';
+import { HeartIcon } from '../../assets/svg';
+import { TitleSection } from '../../components';
+import { BUCKET_URL_ANIMALS } from '../../config/config';
+import ErrorPage from '../Error/ErrorPage';
+import BioDescription from './components/BioDescription';
 import CatDescription from './components/CatDescription';
 import DogDescription from './components/DogDescription';
 import ShelterDescription from './components/ShelterDescription';
-import BioDescription from './components/BioDescription';
-import { BUCKET_URL } from '../../config/config';
+import { animalDetailsQuery, useAnimalDetails } from './useAnimalDetails';
 
-const SectionTitle = ({ title }) => <h3 className="my-5 font-bold">{title}</h3>;
+const SectionTitle = ({ title }) => <h3 className='my-5 font-bold'>{title}</h3>;
 
 export const loader =
-  (queryClient) =>
+  queryClient =>
   async ({ params }) => {
     const { slug } = params;
     await queryClient.ensureQueryData(animalDetailsQuery(slug));
@@ -37,29 +37,29 @@ const AnimalDetailsPage = () => {
     <>
       <TitleSection title={data.name} />
 
-      <main className="flex flex-row justify-center">
-        <section className=" w-[1000px] p-3">
-          <div className="relative ">
-            <Image src={`${BUCKET_URL}/${data.images[0]}`}></Image>
-            <HeartIcon size={40} className="absolute left-3 bottom-3 z-10" />
-            <MinimalLogo size={60} className="absolute right-3 top-3 z-10" />
-            <Button className="absolute right-3 bottom-3 z-10" color="primary">
+      <main className='flex flex-row justify-center'>
+        <section className=' w-[1000px] p-3'>
+          <div className='relative '>
+            <Image src={`${BUCKET_URL_ANIMALS}/${data.images[0]}`}></Image>
+            <HeartIcon size={40} className='absolute left-3 bottom-3 z-10' />
+            <MinimalLogo size={60} className='absolute right-3 top-3 z-10' />
+            <Button className='absolute right-3 bottom-3 z-10' color='primary'>
               Adoptar
             </Button>
           </div>
-          <p className="p-2">{data.description}</p>
+          <p className='p-2'>{data.description}</p>
           {/*<p className="p-2">{data.description} </p>*/}
         </section>
 
         {/* Info */}
-        <section className="p-3">
-          <SectionTitle title="Información" />
+        <section className='p-3'>
+          <SectionTitle title='Información' />
           <ShelterDescription data={data} />
 
-          <SectionTitle title="BIO" />
+          <SectionTitle title='BIO' />
           <BioDescription data={data} />
 
-          <SectionTitle title="Otras características" />
+          <SectionTitle title='Otras características' />
           {data.type === 'cat' && <CatDescription data={data} />}
           {data.type === 'dog' && <DogDescription data={data} />}
         </section>
