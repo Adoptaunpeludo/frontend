a635f689be934d19048126178ced4ba9a73a1364
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-15 22:36:18 +0100

    FIX: Change const url for animal image

--- src/pages/Landing/components/FeaturedPets.jsx
+++ src/pages/Landing/components/FeaturedPets.jsx
@@ -1,17 +1,17 @@
 import { Button, Link } from '@nextui-org/react';
+import { BUCKET_URL_ANIMALS } from '../../../config/config.js';
 import PetCardLanding from '../components/PetCardLanding.jsx';
-import { BUCKET_URL } from '../../../config/config.js';
 const FeaturedPets = ({ title, pets }) => {
   return (
-    <section className="flex flex-col gap-4 h-max z-30  ">
+    <section className='flex flex-col gap-4 h-max z-30  '>
       <header>
-        <h2 className="font-lobster text-5xl text-secondary">{title}</h2>
+        <h2 className='font-lobster text-5xl text-secondary'>{title}</h2>
       </header>
-      <main className="flex flex-col gap-2">
+      <main className='flex flex-col gap-2'>
         {pets.map(({ images, name, age, breed, slug, type }, index) => (
           <PetCardLanding
             key={index}
-            image={`${BUCKET_URL}/${images[0]}`}
+            image={`${BUCKET_URL_ANIMALS}/${images[0]}`}
             name={name}
             age={age}
             breed={breed}
@@ -20,15 +20,15 @@ const FeaturedPets = ({ title, pets }) => {
           />
         ))}
       </main>
-      <footer className="mx-auto">
+      <footer className='mx-auto'>
         <Button
           as={Link}
           href={title === 'Gatetes' ? '/cats' : '/dogs'}
-          size="md"
-          color="primary"
-          className="w-32"
+          size='md'
+          color='primary'
+          className='w-32'
         >
-          <span className="font-lobster text-xl text-white">Ver todos</span>
+          <span className='font-lobster text-xl text-white'>Ver todos</span>
         </Button>
       </footer>
     </section>
