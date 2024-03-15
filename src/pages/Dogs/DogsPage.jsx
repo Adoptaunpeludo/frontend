ea8f89e95959298c5e85e9feff957ba97f0aed6c
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-15 16:05:03 +0100

    feat: add filters to DogsPage

--- src/pages/Dogs/DogsPage.jsx
+++ src/pages/Dogs/DogsPage.jsx
@@ -1,14 +1,33 @@
 import { Spinner } from '@nextui-org/spinner';
-import { PetCard, TitleSection } from '../../components';
-import { useAnimals } from '../Landing/useAnimals';
-//import { Spinner } from "@nextui-org/react";
+import { FilterBar, PetCard, TitleSection } from '../../components';
+import { animalsQuery, useAnimals } from '../Landing/useAnimals';
+import { useLoaderData, useNavigation } from 'react-router-dom';
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const params = Object.fromEntries([
+      ...new URL(request.url).searchParams.entries(),
+    ]);
+
+    await queryClient.ensureQueryData(animalsQuery('dogs', params));
+
+    return { params };
+  };
 
 const DogsPage = () => {
-  const { data, isLoading } = useAnimals('dogs');
+  const { params } = useLoaderData();
+  const navigation = useNavigation();
+
+  const { data } = useAnimals('dogs', params);
+
+  const isLoading = navigation.state === 'loading';
 
   return (
     <>
       <TitleSection title="Perretes" />
+
+      <FilterBar />
 
       <ul className="flex justify-center gap-4 flex-wrap p-6 ">
         {isLoading ? (
