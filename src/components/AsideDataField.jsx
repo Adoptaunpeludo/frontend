49701cac93b78db090a1dc6e001e19b834abbf5a
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-16 14:03:10 +0100

    STYLE: Fix vertical padding

--- src/components/AsideDataField.jsx
+++ src/components/AsideDataField.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 export function AsideDataField({ fieldName, fieldValue }) {
   return (
-    <div className='flex justify-between border-solid border-b-1 border-b-primary mx-2 pb-1'>
+    <div className='flex justify-between border-solid border-b-1 border-b-primary mx-2 py-1'>
       <span>{fieldName}</span>
       <span>{fieldValue}</span>
     </div>
