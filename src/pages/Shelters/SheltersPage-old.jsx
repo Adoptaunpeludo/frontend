401479d1a1ca19d3f0f9529f748b9d3d1820c0db
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-18 22:46:49 +0100

    FEAT: old shelter pages to remove

---
+++ src/pages/Shelters/SheltersPage-old.jsx
@@ -0,0 +1,13 @@
+import { TitleSection, Banner } from "../../components";
+
+const SheltersPage = () => {
+  return (
+    
+  <main>
+        <Banner src={"/backgrounds/banner-shelter.jpg"} />
+        <TitleSection title="SheltersPage"/>
+  
+  </main>);
+};
+
+export default SheltersPage;
