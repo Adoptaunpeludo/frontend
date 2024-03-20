44bf9b2d85a5fd849ea773e3a4cc972586a690a6
Francisco Suárez <paco.max@gmail.com>
2024-03-20 03:59:45 +0100

    Feat: Added size and dimensions validation in ImageUploadModal.jsx image upload.

--- src/pages/Private/shared/components/ImageUploadModal.jsx
+++ src/pages/Private/shared/components/ImageUploadModal.jsx
@@ -19,15 +19,42 @@ const ImageUploadModal = () => {
   const queryClient = useQueryClient();
   const [selectedFile, setSelectedFile] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [error, setError] = useState();
+  const [fileError, setFileError] = useState();
 
   const maxSizeInBytes = 1 * 1024 * 1024; // 1 MB
   const maxWidth = 1920;
   const maxHeight = 1080;
 
+  const handleClose = (onClose) => {
+    setFileError('');
+    onClose();
+  };
+
+  const imageFileValidation = (event, maxSize, maxW, maxH) => {
+    const file = event.target.files.item(0);
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+
+    image.onload = () => {
+      if (image.width > maxW || image.height > maxH) {
+        setFileError(
+          `La imagen excede las dimensiones admitidas (${maxW}x${maxH} px)`
+        );
+      } else {
+        if (file.size > maxSize) {
+          setFileError(
+            `La imagen excede el tamaño de archivo permitido (${maxSize} bytes)`
+          );
+        } else {
+          setFileError('');
+          setSelectedFile(event.target.files.item(0));
+        }
+      }
+    };
+  };
+
   const handleUploadFile = async (onClose) => {
-    if (!selectedFile) return;
+    if (!selectedFile || fileError) return;
 
     try {
       setIsLoading(true);
@@ -41,6 +68,8 @@ const ImageUploadModal = () => {
       toast.error('Error al subir el archivo');
       console.log(error);
     } finally {
+      setSelectedFile();
+      setFileError('');
       setIsLoading(false);
     }
   };
@@ -73,34 +102,21 @@ const ImageUploadModal = () => {
                         className="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
                         id="file_input"
                         type="file"
+                        style={
+                          fileError ? { color: 'red' } : { color: 'black' }
+                        }
                         onChange={(e) => {
-                          setError('');
-                          const file = e.target.files.item(0);
-                          const image = new Image();
-                          image.src = URL.createObjectURL(file);
-
-                          image.onload = () => {
-                            if (
-                              image.width > maxWidth ||
-                              image.height > maxHeight
-                            ) {
-                              setIsDisabled(true);
-                              setError(
-                                'Las dimensiones de la imagen son demasiado grandes.'
-                              );
-                              return;
-                            }
-                          };
-
-                          if (file.size > maxSizeInBytes) {
-                            setIsDisabled(true);
-                            console.log('file too big');
-                          } else {
-                            return setSelectedFile(e.target.files.item(0));
-                          }
+                          imageFileValidation(
+                            e,
+                            maxSizeInBytes,
+                            maxWidth,
+                            maxHeight
+                          );
                         }}
                       />
-                      <p color="red">{error}</p>
+                      <p style={{ color: 'red', fontSize: '16px' }}>
+                        {fileError}
+                      </p>
                     </>
                   )}
                 </ModalBody>
@@ -108,7 +124,7 @@ const ImageUploadModal = () => {
                   <Button
                     color="default"
                     variant="light"
-                    onPress={onClose}
+                    onPress={() => handleClose(onClose)}
                     disabled={isLoading}
                   >
                     Cerrar
@@ -116,7 +132,7 @@ const ImageUploadModal = () => {
                   <Button
                     className="bg-tertiary text-white"
                     onPress={() => handleUploadFile(onClose)}
-                    disabled={isLoading || isDisabled}
+                    disabled={isLoading}
                   >
                     Subir
                   </Button>
