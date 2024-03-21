67d0759523301b9b1b9ebae81b17edd90b01ad2d
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-21 21:16:50 +0100

    FIX: Remove hero banner

--- src/pages/Public/Animals/AnimalsPage.jsx
+++ src/pages/Public/Animals/AnimalsPage.jsx
@@ -1,16 +1,11 @@
 import { useLoaderData } from 'react-router';
 import { useNavigation } from 'react-router-dom';
 
-import {
-  Banner,
-  FilterBar,
-  PagePagination,
-  PetCard,
-  TitleSection,
-} from '../../../components';
+import { FilterBar, PagePagination, TitleSection } from '../../../components';
 
-import { animalsQuery, useAnimals } from '../Landing/useAnimals';
 import { Skeleton } from '@nextui-org/react';
+import { animalsQuery, useAnimals } from '../Landing/useAnimals';
+import { PetCard } from './components/PetCard';
 
 export const loader =
   (queryClient, page) =>
@@ -36,24 +31,21 @@ const AnimalsPage = ({ page }) => {
   const isLoading = navigation.state === 'loading';
 
   return (
-    <>
-      <Banner src={`/backgrounds/banner-${page}.jpg`} />
-      <main className="max-w-screen-xl w-full flex  flex-col justify-center  gap-12 h-full  py-12  mx-auto flex-grow">
-        <TitleSection title={page === 'cats' ? 'Gatetes' : 'Perretes'} />
+    <main className="max-w-screen-xl w-full flex  flex-col justify-center  gap-12 h-full  py-12  mx-auto flex-grow">
+      <TitleSection title={page === 'cats' ? 'Gatetes' : 'Perretes'} />
 
-        <FilterBar page={page} />
-        <ul className="flex justify-center gap-4 flex-wrap p-6">
-          {data.animals.map((animal) => (
-            <Skeleton isLoaded={!isLoading} key={animal.id}>
-              <PetCard key={animal.id} animal={animal} />
-            </Skeleton>
-          ))}
-        </ul>
-        <footer className="mx-auto">
-          <PagePagination data={data} />
-        </footer>
-      </main>
-    </>
+      <FilterBar page={page} />
+      <ul className="flex justify-center gap-4 flex-wrap p-6">
+        {data.animals.map((animal) => (
+          <Skeleton isLoaded={!isLoading} key={animal.id}>
+            <PetCard key={animal.id} animal={animal} />
+          </Skeleton>
+        ))}
+      </ul>
+      <footer className="mx-auto">
+        <PagePagination data={data} />
+      </footer>
+    </main>
   );
 };
 
