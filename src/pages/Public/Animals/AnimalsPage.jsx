a41d7c34c778965fbb536e91d839c8626b7fdfa5
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-21 13:06:47 +0100

    refactor: remove argument for page in loader and hook useShelters to fix queryKey, handle isLoading state with navigation, add Skeleton to ShelterCard while loading

--- src/pages/Public/Animals/AnimalsPage.jsx
+++ src/pages/Public/Animals/AnimalsPage.jsx
@@ -9,7 +9,6 @@ import {
   TitleSection,
 } from '../../../components';
 
-import { useRef } from 'react';
 import { animalsQuery, useAnimals } from '../Landing/useAnimals';
 import { Skeleton } from '@nextui-org/react';
 
@@ -22,6 +21,7 @@ export const loader =
 
     if (params.name) params.name = params.name.toLowerCase();
 
+    //TODO: TryCatch
     await queryClient.ensureQueryData(animalsQuery(page, params));
 
     return { params };
@@ -30,24 +30,17 @@ export const loader =
 const AnimalsPage = ({ page }) => {
   const { params } = useLoaderData();
   const navigation = useNavigation();
-  const titleRef = useRef(null);
 
   const { data } = useAnimals(page, params);
 
   const isLoading = navigation.state === 'loading';
-
-  // useEffect(() => {
-  //   if (!isLoading) {
-  //     titleRef.current.scrollIntoView({ behavior: 'instant' });
-  //   }
-  // }, [isLoading]);
 
   return (
     <Skeleton isLoaded={!isLoading}>
       <Banner src={`/backgrounds/banner-${page}.jpg`} />
       <main className="max-w-screen-xl w-full flex  flex-col justify-center  gap-12 h-full  py-12  mx-auto flex-grow">
         <TitleSection title={page === 'cats' ? 'Gatetes' : 'Perretes'} />
-        <div ref={titleRef}></div>
+
         <FilterBar page={page} />
         <ul className="flex justify-center gap-4 flex-wrap p-6">
           {data.animals.map((animal) => (
