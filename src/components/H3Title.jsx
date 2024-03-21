1b31b8543769e49b81960095274eae5ce1118341
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-21 22:45:56 +0100

    Fix: Styles

--- src/components/H3Title.jsx
+++ src/components/H3Title.jsx
@@ -2,12 +2,12 @@ import PropTypes from 'prop-types';
 
 export function H3Title({ title, className }) {
   return (
-    <h3 className={` ${className} font-poppins text-base font-semibold`}>
+    <h3 className={` font-poppins text-base font-semibold mx-2 ${className}`}>
       {title}
     </h3>
   );
 }
 
 H3Title.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
 };
