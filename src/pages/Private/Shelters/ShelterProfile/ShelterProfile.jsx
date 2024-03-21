2e331a392f8f663c109a181ead14b001e49f96c5
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-21 09:23:30 +0100

    refactor: UserBioInfo into a smaller component

--- src/pages/Private/Shelters/ShelterProfile/ShelterProfile.jsx
+++ src/pages/Private/Shelters/ShelterProfile/ShelterProfile.jsx
@@ -1,27 +1,16 @@
-import { Avatar, Button, Spinner, User } from '@nextui-org/react';
+import { Button, Spinner } from '@nextui-org/react';
 import { IconEdit, IconTrashXFilled } from '@tabler/icons-react';
-
-import {
-  AsideDataColumn,
-  CameraIcon,
-  H2Title,
-  Hero,
-  TitleSection,
-} from '../../../../components';
-
-import { StatusAnimalsTable, UserFormBio, userInformation } from '../../shared';
-
-import { BUCKET_URL } from '../../../../config/config';
-
+import { Hero, TitleSection } from '../../../../components';
+import { StatusAnimalsTable } from '../../shared';
 import { toast } from 'react-toastify';
 import { updateShelterProfile } from '../ShelterForm/service';
-
 import { useUser } from '../../useUser';
 import { useAnimalImagesContext } from '../../../../context/AnimalImagesContext';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { deleteAnimal } from '../AnimalForm/service';
 import ShelterProfileInfo from './components/ShelterProfileInfo';
+import UserBioInfo from './components/UserBioInfo';
 
 export const action =
   (closeBioModal, closeShelterModal, queryClient) =>
@@ -71,8 +60,7 @@ const ShelterProfile = () => {
 
   if (isLoading) return <Spinner />;
 
-  const { username, avatar } = data;
-  const userData = userInformation(data);
+  const { username } = data;
 
   return (
     <main className="bg-default-100 flex-grow">
@@ -87,40 +75,21 @@ const ShelterProfile = () => {
           <main className="flex flex-col max-w-3xl order-1 max-lg:order-2">
             <ShelterProfileInfo isLoading={isFetching} data={data} />
           </main>
-          <aside className="w-96 flex flex-col order-2 max-lg:order-1 mx-auto">
-            <div id="profileAside" className=" flex flex-col gap-5">
-              <Avatar
-                isBordered
-                color="success"
-                className="w-40 h-40 bg-white self-center"
-                src={`${BUCKET_URL}/${avatar}`}
-                showFallback
-                fallback={<CameraIcon />}
+          <UserBioInfo data={data} isLoading={isFetching} />
+          {/* <div id="NotificationsAside">
+            <H2Title title="Mensajes" className="pb-5" />
+            <div className="flex justify-between border-solid border-b-1 border-b-primary pb-3 items-center">
+              <User
+                name="Jane Doe"
+                avatarProps={{
+                  src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
+                  isBordered: true,
+                  color: 'success',
+                }}
               />
-              <div
-                id="personalData"
-                className="flex flex-col justify-start gap-4"
-              >
-                <H2Title title="Información" />
-                <AsideDataColumn dataColumn={userData} />
-              </div>
+              {1}
             </div>
-            <UserFormBio data={data} />
-            <div id="NotificationsAside">
-              <H2Title title="Mensajes" className="pb-5" />
-              <div className="flex justify-between border-solid border-b-1 border-b-primary pb-3 items-center">
-                <User
-                  name="Jane Doe"
-                  avatarProps={{
-                    src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
-                    isBordered: true,
-                    color: 'success',
-                  }}
-                />
-                {1}
-              </div>
-            </div>
-          </aside>
+          </div> */}
         </section>
         <section id="petsTable" className="px-4">
           <StatusAnimalsTable role={'shelter'} />
