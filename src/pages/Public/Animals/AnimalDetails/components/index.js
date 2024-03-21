63334a0038810b4ed85096a9113f8f8d060f53cd
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-21 19:24:50 +0100

    Refactor: remove import for shared component moved

--- src/pages/Public/Animals/AnimalDetails/components/index.js
+++ src/pages/Public/Animals/AnimalDetails/components/index.js
@@ -1,4 +1,2 @@
 export { AnimalFavs } from './AnimalFavs';
-export { AnimalGallery } from './AnimalGallery';
-export { ContactShelter } from './ContactShelter';
 export { ShareSocialMedia } from './ShareSocialMedia';
