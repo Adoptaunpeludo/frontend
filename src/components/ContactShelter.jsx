df4fc7d7ed040b3e0f09ea747a06b8ccfd87e7a5
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-22 21:11:54 +0100

    FIX: Change chat icon component

--- src/components/ContactShelter.jsx
+++ src/components/ContactShelter.jsx
@@ -1,13 +1,16 @@
-import { IconMessages } from '@tabler/icons-react';
 import { H2Title } from '.';
+import { ChatIcon } from '../assets/svg';
 export const ContactShelter = ({ className }) => {
   return (
-    <section id="talk-to-shelters" className={`flex items-center ${className}`}>
+    <section
+      id="talk-to-shelters"
+      className={`flex items-center ${className} `}
+    >
       <H2Title
         title="habla con la protectora"
         className="text-secondary max-sm:w-48"
       />
-      <IconMessages className=" stroke-slate-100 fill-black size-14" />
+      <ChatIcon className="size-14" />
     </section>
   );
 };
