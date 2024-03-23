32fcedd916b1465b31b2d82dca781d998802eea6
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-23 13:56:25 +0100

    Feat: add custom hook for shelters-animals

--- src/pages/Public/Shelters/ShelterDetails/useShelterDetails.js
+++ src/pages/Public/Shelters/ShelterDetails/useShelterDetails.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { getShelterDetails } from './service';
+import { getShelterAnimals, getShelterDetails } from './service';
 
 export const shelterDetailsQuery = (username) => {
   return {
@@ -13,4 +13,18 @@ export const useShelterDetails = (username) => {
   const { data, isLoading, isError } = useQuery(shelterDetailsQuery(username));
   const shelter = data.users[0];
   return { data: shelter, isLoading, isError };
+};
+
+export const shelterAnimalsQuery = (shelterId) => {
+  return {
+    queryKey: ['shelter-animals', shelterId],
+    queryFn: () => getShelterAnimals(shelterId),
+    retry: false,
+  };
+};
+
+export const useShelterAnimals = (username) => {
+  const { data, isLoading, isError } = useQuery(shelterAnimalsQuery(username));
+
+  return { data, isLoading, isError };
 };
