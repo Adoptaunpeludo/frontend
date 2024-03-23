b9957901e5bf284497b6ae9fa328a0c223749af6
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-24 00:33:25 +0100

    refactor: delete / in getShelterDetails hook, change endpoint from createdBy to shelterName

--- src/pages/Public/Shelters/ShelterDetails/service.js
+++ src/pages/Public/Shelters/ShelterDetails/service.js
@@ -1,12 +1,12 @@
 import { fetchData } from '../../../../api/client';
 
 export const getShelterDetails = async (username) => {
-  const { data } = await fetchData(`/users/?username=${username}`);
+  const { data } = await fetchData(`/users?username=${username}`);
   return data;
 };
 
-export const getShelterAnimals = async (shelterId) => {
-  const { data } = await fetchData(`/animals?createdBy=${shelterId}`);
+export const getShelterAnimals = async (shelterName) => {
+  const { data } = await fetchData(`/animals?shelterName=${shelterName}`);
 
   return data;
 };
