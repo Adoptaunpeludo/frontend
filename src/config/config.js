e12b89a5d88764944acfae0814cd70000e2a6995
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-23 02:58:19 +0100

    fix: WB_SERVER url to wss

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const BUCKET_URL = 'https://aup-s3images.s3.eu-west-3.amazonaws.com';
 export const FALLBACK_IMAGE_CARD = '/no-image-card.jpg';
-export const WB_SERVER = 'wss://websocket.adoptaunpeludo.com/websocket';
+export const WB_SERVER = 'wss://websocket.adoptaunpeludo.com/';
