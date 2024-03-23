0980d234bb7a058fc1bdea6b3282c4176fa571d5
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-23 13:54:30 +0100

    Feat: add query for status animals of shelter

--- src/pages/Public/Shelters/ShelterDetails/service.js
+++ src/pages/Public/Shelters/ShelterDetails/service.js
@@ -2,6 +2,11 @@ import { fetchData } from '../../../../api/client';
 
 export const getShelterDetails = async (username) => {
   const { data } = await fetchData(`/users/?username=${username}`);
+  return data;
+};
+
+export const getShelterAnimals = async (shelterId) => {
+  const { data } = await fetchData(`/animals?createdBy=${shelterId}`);
 
   return data;
 };
