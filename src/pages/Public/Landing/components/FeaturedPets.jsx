be85a326a79690185e031c46f4af99f024f5aabe
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-25 22:41:42 +0100

    Feat: change feature to show

--- src/pages/Public/Landing/components/FeaturedPets.jsx
+++ src/pages/Public/Landing/components/FeaturedPets.jsx
@@ -1,6 +1,6 @@
 import { Button, Link } from '@nextui-org/react';
-import PetCardLanding from '../components/PetCardLanding.jsx';
 import { BUCKET_URL } from '../../../../config/config.js';
+import PetCardLanding from '../components/PetCardLanding.jsx';
 const FeaturedPets = ({ title, pets }) => {
   return (
     <section className="flex flex-col gap-4 h-max z-30  ">
@@ -8,13 +8,13 @@ const FeaturedPets = ({ title, pets }) => {
         <h2 className="font-lobster text-5xl text-secondary">{title}</h2>
       </header>
       <main className="flex flex-col gap-2">
-        {pets.map(({ images, name, age, breed, slug, type }, index) => (
+        {pets.map(({ images, name, age, gender, slug, type }, index) => (
           <PetCardLanding
             key={index}
             image={`${BUCKET_URL}/${images[0]}`}
             name={name}
+            gender={gender}
             age={age}
-            breed={breed}
             slug={slug}
             type={type}
           />
