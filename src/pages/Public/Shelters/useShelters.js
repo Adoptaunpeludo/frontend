e51e57b0190e32f27757b35655a704f4deea1e5a
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-25 23:41:44 +0100

    fix: add page to sheltersQuery queryKey

--- src/pages/Public/Shelters/useShelters.js
+++ src/pages/Public/Shelters/useShelters.js
@@ -2,10 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { getShelters } from './service';
 
 export const sheltersQuery = (params = {}) => {
-  const { username, city, limit } = params;
+  const { username, city, limit, page } = params;
 
   return {
-    queryKey: ['shelters', username, city, limit],
+    queryKey: ['shelters', username, city, limit, page],
     queryFn: () => getShelters(params),
     staleTime: 1000 * 60 * 15,
   };
