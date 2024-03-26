0fdc0f3af2b5518ab2c4e00b407d704dd3016723
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-26 17:22:41 +0100

    Feat: apply basetypografy and capitalize style

--- src/components/AsideDataField.jsx
+++ src/components/AsideDataField.jsx
@@ -1,8 +1,8 @@
 export function AsideDataField({ fieldName, fieldValue }) {
   return (
-    <div className="flex justify-between border-solid border-b-1 border-b-primary mx-2 pt-2 pb-1">
+    <div className="flex justify-between border-solid border-b-1 border-b-primary mx-2 pt-2 pb-1 font-poppins">
       <span>{fieldName}</span>
-      <span>{fieldValue}</span>
+      <span className="capitalize">{fieldValue}</span>
     </div>
   );
 }
