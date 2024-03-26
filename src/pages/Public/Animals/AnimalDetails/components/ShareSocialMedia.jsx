c5fc806e2b27296146e7f61339315687e3208c73
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-26 19:34:15 +0100

    Feat: add specific links to several social media

--- src/pages/Public/Animals/AnimalDetails/components/ShareSocialMedia.jsx
+++ src/pages/Public/Animals/AnimalDetails/components/ShareSocialMedia.jsx
@@ -1,16 +1,42 @@
+import { Link } from '@nextui-org/react';
 import {
   IconBrandFacebook,
-  IconBrandInstagram,
+  IconBrandLinkedin,
   IconBrandX,
 } from '@tabler/icons-react';
 import { H2Title } from '../../../../../components';
-export const ShareSocialMedia = () => {
+export const ShareSocialMedia = ({ url }) => {
   return (
     <section id="share-rrss" className="flex items-center justify-start">
       <H2Title title="compárteme" className="text-secondary" />
-      <IconBrandInstagram className="size-12" />
-      <IconBrandFacebook className="size-12" />
-      <IconBrandX className="size-12" />
+      <Link
+        isExternal
+        isBlock
+        href={`https://www.linkedin.com/shareArticle?mini=true&url=${url}&summary=%C2%A1Dale%20un%20hogar%20y%20amor%20a%20un%20amigo%20peludo%C2%A1`}
+        target="_blank"
+        color="foreground"
+      >
+        <IconBrandLinkedin className="size-12" />
+      </Link>
+      <Link
+        isExternal
+        isBlock
+        href={`https://www.facebook.com/sharer/sharer.php?u=${url}
+`}
+        target="_blank"
+        color="foreground"
+      >
+        <IconBrandFacebook className="size-12" />
+      </Link>
+      <Link
+        isExternal
+        isBlock
+        href={`https://twitter.com/intent/tweet?text=%C2%A1Dale%20un%20hogar%20y%20amor%20a%20un%20amigo%20peludo%C2%A1&url=${url}`}
+        target="_blank"
+        color="foreground"
+      >
+        <IconBrandX className="size-12" />
+      </Link>
     </section>
   );
 };
