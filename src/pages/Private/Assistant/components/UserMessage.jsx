2b39eefce7f74704c17871824a8c2764c0c48e51
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-26 23:42:15 +0100

    Fix: Avatar image pending add fallback image

--- src/pages/Private/Assistant/components/UserMessage.jsx
+++ src/pages/Private/Assistant/components/UserMessage.jsx
@@ -1,16 +1,17 @@
 import { Avatar } from '@nextui-org/react';
+import { BUCKET_URL } from '../../../../config/config';
 import { useUser } from '../../useUser';
 
 const UserMessage = ({ text }) => {
   const { data: user } = useUser();
-
+  console.log(user.avatar);
   return (
     <div className="sm:col-start-6 col-start-1 col-end-13 p-3 rounded-lg">
       <div className="flex items-center justify-start flex-row-reverse">
         {/* <div className="flex items-center justify-center h-10 w-10 rounded-full bg-tertiary-500 flex-shrink-0 text-white">
           U
         </div> */}
-        <Avatar src={user.avatar} />
+        <Avatar src={`${BUCKET_URL}/${user?.avatar}`} />
         <div className="relative mr-3 text-medium bg-tertiary py-2 px-4 rounded-xl text-white shadow-xl">
           {text}
         </div>
