6c28f88dc1a363aa6e879694c3a3163121829b20
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-26 22:23:31 +0100

    fix: add context to ProtectedRoute Outlet

--- src/pages/Private/ProtectedRoute.jsx
+++ src/pages/Private/ProtectedRoute.jsx
@@ -1,9 +1,13 @@
 import { Navigate, Outlet, useOutletContext } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  const { user } = useOutletContext();
+  const { user, notifications } = useOutletContext();
 
-  return user ? <Outlet /> : <Navigate to={'/login'} />;
+  return user ? (
+    <Outlet context={{ user, notifications }} />
+  ) : (
+    <Navigate to={'/login'} />
+  );
 };
 
 export default ProtectedRoute;
