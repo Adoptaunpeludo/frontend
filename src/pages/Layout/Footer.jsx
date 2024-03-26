3e76bcf1bd1818ddb33aa82a7075e2a15e322475
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-26 23:40:51 +0100

    feat: Add fixed height

--- src/pages/Layout/Footer.jsx
+++ src/pages/Layout/Footer.jsx
@@ -1,21 +1,21 @@
 import {
   IconBrandFacebook,
   IconBrandInstagram,
-  IconBrandX
+  IconBrandX,
 } from '@tabler/icons-react';
 import Copyright from '../../assets/copyright.svg?react';
 
 const Footer = () => {
   return (
-    <div className=' h-16 w-full bg-primary'>
-      <div className='max-w-screen-lg w-full mx-auto flex justify-between items-center h-full'>
-        <div id='DevelopedBy' className=''>
-          <Copyright className='w-60' />
+    <div className=" h-[5vh] w-full bg-primary">
+      <div className="max-w-screen-lg w-full mx-auto flex justify-between items-center h-full">
+        <div id="DevelopedBy" className="">
+          <Copyright className="w-60" />
         </div>
-        <div id='SocialIcons' className='flex justify-center '>
-          <IconBrandX className='size-9' />
-          <IconBrandInstagram className='size-9' />
-          <IconBrandFacebook className='size-9' />
+        <div id="SocialIcons" className="flex justify-center ">
+          <IconBrandX className="size-9" />
+          <IconBrandInstagram className="size-9" />
+          <IconBrandFacebook className="size-9" />
         </div>
       </div>
     </div>
