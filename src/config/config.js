68c5848d0f438148a735b3d77718ae0d3b4df00d
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-27 15:50:33 +0100

    feat: add assistant-service devlopment address to config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export const BUCKET_URL = 'https://aup-s3images.s3.eu-west-3.amazonaws.com';
 export const FALLBACK_IMAGE_CARD = '/no-image-card.jpg';
-// export const WB_SERVER = 'wss://websocket.adoptaunpeludo.com/';
-export const WB_SERVER = 'ws://localhost:3070';
+export const WB_SERVER = 'wss://websocket.adoptaunpeludo.com/';
+// export const WB_SERVER = 'ws://localhost:3070';
 export const ASSISTANT_SERVER = 'https://www.adoptaunpeludo.com/assistant';
+// export const ASSISTANT_SERVER = 'http://localhost:3777/api/chat';
