1aff1620ebefb1e6163cb7385ef057d6e52ff6ae
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-03-27 22:18:08 +0100

    FIX: change prop disable for readonly

--- src/pages/Private/Shelters/ShelterProfile/components/Acommodations.jsx
+++ src/pages/Private/Shelters/ShelterProfile/components/Acommodations.jsx
@@ -3,11 +3,11 @@ import { Checkbox } from '@nextui-org/react';
 import { H4Title } from '../../../../../components';
 import { facilitiesEnum } from '../../../../../utils/enumData';
 
-const Accommodations = ({ facilities, isDisabled = true }) => {
+const Accommodations = ({ facilities, isDisabled = true, className }) => {
   return (
     <div
       id="accommodations"
-      className="flex flex-col gap-2 rounded-lg bg-default-100 py-2"
+      className={`flex flex-col gap-2 rounded-lg py-2 font-poppins ${className}`}
     >
       <H4Title title="Alojamientos" />
       <div id="accommodationsCheck" className="flex flex-wrap gap-5 mx-7">
@@ -17,7 +17,7 @@ const Accommodations = ({ facilities, isDisabled = true }) => {
             radius="none"
             size="sm"
             defaultSelected={facilities.includes(facility.value)}
-            isDisabled={isDisabled}
+            isReadOnly={isDisabled}
             defaultValue={facilities.includes(facility.value)}
             name="facilities"
             value={facility.value}
