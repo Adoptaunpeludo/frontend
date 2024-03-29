e8f001a917e0d4e21aa68b0936b52fd953fe3ef0
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-03-29 08:43:40 +0100

    feat: AdoptionChatContextProvider wrapper in AppLayout

--- src/pages/Layout/AppLayout.jsx
+++ src/pages/Layout/AppLayout.jsx
@@ -11,6 +11,7 @@ import { userQuery } from '../Private/useUser';
 import { userNotificationsQuery } from '../Private/useNotifications';
 import { WebSocketContextProvider } from '../../context/WebSocketContext';
 import { NotificationsContextProvider } from '../../context/NotificationsContext';
+import { AdoptionChatContextProvider } from '../../context/AdoptionChatContext';
 
 export const loader = (queryClient) => async () => {
   try {
@@ -32,13 +33,15 @@ const AppLayout = () => {
     <>
       <NextUIProvider navigate={navigate}>
         <div className="min-h-screen flex flex-col">
-          <NotificationsContextProvider>
-            <WebSocketContextProvider user={user}>
-              <Header />
-              <Outlet context={{ user, notifications }} />
-              <Footer />
-            </WebSocketContextProvider>
-          </NotificationsContextProvider>
+          <AdoptionChatContextProvider>
+            <NotificationsContextProvider>
+              <WebSocketContextProvider user={user}>
+                <Header />
+                <Outlet context={{ user, notifications }} />
+                <Footer />
+              </WebSocketContextProvider>
+            </NotificationsContextProvider>
+          </AdoptionChatContextProvider>
         </div>
         <ScrollRestoration />
       </NextUIProvider>
