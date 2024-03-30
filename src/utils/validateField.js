c9c7656095d18156830e392e6e212e6a849e56a8
Francisco Suárez <paco.max@gmail.com>
2024-03-30 04:14:03 +0100

    FIX: changed phone min char from 6 to 8

--- src/utils/validateField.js
+++ src/utils/validateField.js
@@ -35,7 +35,7 @@ export const validateField = (name, value, password) => {
         ? 'El DNI no es válido.'
         : '';
     case 'phoneNumber':
-      return !/^(\+[0-9]{1,3})?[0-9]{6,14}$/.test(value.trim())
+      return !/^(\+[0-9]{1,3})?[0-9]{8,14}$/.test(value.trim())
         ? 'No es un número de teléfono válido.'
         : '';
     default:
