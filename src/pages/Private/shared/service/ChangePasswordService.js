12fba58f16cec3e678899b537d9761a059e1b5ac
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-02 22:32:45 +0200

    FIX: remove Console.log

--- src/pages/Private/shared/service/ChangePasswordService.js
+++ src/pages/Private/shared/service/ChangePasswordService.js
@@ -4,9 +4,6 @@ export const updatePassword = async (formData) => {
   const updatedData = Object.fromEntries(formData);
   delete updatedData.intent;
   delete updatedData.repeatPassword;
-  console.log({ updatedData }, 'esto mando');
-
   const { data } = await updateData('users/me/change-password', updatedData);
-
   return data;
 };
