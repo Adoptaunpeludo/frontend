ffc094984d009f4c3cbe7d3362e3761344de3eb6
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-02 22:23:58 +0200

    Feat: Change cleaning Data to back

--- src/pages/Private/shared/service/ChangePasswordService.js
+++ src/pages/Private/shared/service/ChangePasswordService.js
@@ -1,9 +1,12 @@
 import { updateData } from '../../../../api/client';
 
-export const updatePassword = async (passwordPair) => {
-  const { data } = await updateData('users/me/change-password', {
-    passwordPair,
-  });
+export const updatePassword = async (formData) => {
+  const updatedData = Object.fromEntries(formData);
+  delete updatedData.intent;
+  delete updatedData.repeatPassword;
+  console.log({ updatedData }, 'esto mando');
+
+  const { data } = await updateData('users/me/change-password', updatedData);
 
   return data;
 };
