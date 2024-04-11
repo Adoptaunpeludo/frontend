8849374271060cf20b359ebdcaf60bf8291c3244
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-11 22:07:45 +0200

    Feat: apply selective styles for pet status field

--- src/components/AsideDataField.jsx
+++ src/components/AsideDataField.jsx
@@ -1,8 +1,26 @@
+import { statusColorMap } from '../utils/enumData';
+
 export function AsideDataField({ fieldName, fieldValue }) {
   return (
-    <div className="flex justify-between border-solid border-b-1 border-b-primary mx-2 pt-2 pb-1 font-poppins">
-      <span>{fieldName}</span>
-      <span className={`${fieldName !== 'Email: ' ? 'capitalize' : ''}`}>
+    <div
+      className={`flex  ${
+        fieldName === 'Estado:'
+          ? 'justify-end'
+          : 'justify-between border-solid border-b-1 border-b-primary'
+      } mx-2 pt-2 pb-1 font-poppins items-center`}
+    >
+      <span className={` ${fieldName === 'Estado:' ? 'hidden' : ''}`}>
+        {fieldName}
+      </span>
+      <span
+        className={`${fieldName !== 'Email: ' ? 'capitalize' : ''} ${
+          fieldName === 'Estado:'
+            ? 'bg-' + statusColorMap[fieldValue] + ' px-2 rounded-md py-2'
+            : ''
+        }
+        ${fieldValue === 'adoptado' ? 'text-white' : ''}
+        `}
+      >
         {fieldValue}
       </span>
     </div>
