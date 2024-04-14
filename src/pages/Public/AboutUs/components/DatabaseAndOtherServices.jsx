306779b591e3e1b6e67b632554cfc25e23f30da3
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-14 23:48:07 +0200

    Refactor: Change layout for database component

--- src/pages/Public/AboutUs/components/DatabaseAndOtherServices.jsx
+++ src/pages/Public/AboutUs/components/DatabaseAndOtherServices.jsx
@@ -1,33 +1,40 @@
-import { Button, Link } from '@nextui-org/react';
+import { Button, Image, Link } from '@nextui-org/react';
 
 export const DatabaseAndOtherServices = () => {
   return (
-    <section className="w-full min-h-80 bg-white flex flex-col justify-center mx-auto px-10">
+    <section className="w-full min-h-80  flex flex-col justify-center mx-auto px-10">
       <section className=" max-w-screen-xl w-full   mx-auto">
-        <header className="flex justify-start font-lobster text-5xl  text-secondary py-10">
-          Bases de datos y otros servicios
+        <header className="flex justify-start font-lobster text-5xl  text-secondary py-10 ">
+          Diagrama Entidad-Relación de la Base de Datos
         </header>
-        <main className="flex justify-start  flex-col gap-5 max-w-screen-md">
-          <article className="text-pretty font-poppins leading-6">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            pellentesque tempus lacinia. Sed mattis in magna non malesuada.
-            Suspendisse eu nisl sodales purus malesuada lacinia. Aliquam nec
-            odio a justo pellentesque semper vitae molestie ipsum. Ut semper
-            commodo maximus. Duis egestas sed justo eget molestie. Vestibulum
-            ante ipsum primis in faucibus orci luctus et ultrices posuere
-            cubilia curae; Mauris dui augue, finibus et nulla a, ornare congue
-            ex. Aenean magna mauris, consequat ac nisl ut, ornare varius mauris.
-            Fusce dui diam, maximus eget vestibulum vitae, cursus eu leo.
-            Curabitur aliquet dapibus lacus lobortis pharetra. Pellentesque
-            lacus massa, varius nec pretium nec, sagittis id massa. Nam id
-            ultrices purus, ullamcorper pellentesque dui. Pellentesque ligula
-            lorem, auctor consectetur justo id, semper accumsan dolor.
+        <main className="flex justify-end gap-5 w-full flex-col">
+          <article className="text-pretty font-poppins leading-6  max-w-screen-md ">
+            Explora la estructura y las relaciones de nuestra base de datos
+            mediante el diagrama entidad-relación, que ofrece una representación
+            visual clara y concisa de su diseño.
+          </article>
+
+          <article className=" max-w-screen-xl w-full flex flex-col justify-center mx-auto mb-5">
+            <Link
+              href="https://app.eraser.io/workspace/2zsIHy4kYoNR06OS28Er?elements=3usosqcYz6oZYbeadIwRVw"
+              isExternal
+            >
+              <figure>
+                <figcaption className="border-b-1 border-primary text-lg font-poppins text-foreground mb-5 pb-3 font-semibold">
+                  Diagrama entidad relación de la base de datos
+                </figcaption>
+                <Image src="https://app.eraser.io/workspace/2zsIHy4kYoNR06OS28Er/preview?elements=3usosqcYz6oZYbeadIwRVw&type=embed" />
+              </figure>
+            </Link>
           </article>
         </main>
-        <footer className="py-10">
-          <Link href="http://www.google.es" target="_blank">
-            <Button className="font-lobster text-3xl bg-primary text-white py-6 px-5 capitalize">
-              Ver diagrama
+        <footer className="py-10 flex justify-end">
+          <Link
+            href="https://app.eraser.io/workspace/2zsIHy4kYoNR06OS28Er?elements=3usosqcYz6oZYbeadIwRVw"
+            isExternal
+          >
+            <Button className="font-lobster text-2xl bg-primary text-white py-6 px-5 capitalize">
+              Explorar Diagrama E-R
             </Button>
           </Link>
         </footer>
