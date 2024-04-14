a60c9eb470e3f2e17d521bd99a6f1f3643f7671d
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-14 14:02:45 +0200

    Feat: refactor to extract team information

--- src/pages/Public/AboutUs/components/OurTeam.jsx
+++ src/pages/Public/AboutUs/components/OurTeam.jsx
@@ -6,55 +6,7 @@ import {
   IconMail,
 } from '@tabler/icons-react';
 import { H2Title, H3Title } from '../../../../components';
-
-const team = [
-  {
-    img: '/public/logoTech/aws.svg',
-    person: 'José Alberto Delgado',
-    position: 'Backend Lead',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra dui ac ante viverra vestibulum. Suspendisse non orci eget ipsum venenatis egestas ut a odio. Suspendisse finibus placerat lectus vel vulputate. Sed sodales a est et sodales. In hac habitasse platea dictumst. Sed velit neque, imperdiet sed fringilla mattis, consequat nec ligula. Praesent molestie libero nibh, sed sollicitudin dolor faucibus ut.',
-    link: 'dsds',
-    github: 'https://github.com/JoseAlbDR',
-    linkdin: 'https://www.linkedin.com/in/jalbertodelgado/',
-    mailto: 'josealbdr84@gmail.com',
-  },
-  {
-    img: '/public/logoTech/aws.svg',
-    person: 'Juan Manuel Acosta',
-    position: 'UX/UI, Fronted dev ',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra dui ac ante viverra vestibulum. Suspendisse non orci eget ipsum venenatis egestas ut a odio. Suspendisse finibus placerat lectus vel vulputate. Sed sodales a est et sodales. In hac habitasse platea dictumst. Sed velit neque, imperdiet sed fringilla mattis, consequat nec ligula. Praesent molestie libero nibh, sed sollicitudin dolor faucibus ut.',
-    link: 'dsds',
-    github: 'https://github.com/jmacosta',
-    linkdin: 'https://www.linkedin.com/in/juan-manuel-acosta-benitez/',
-    mailto: 'mailto:jmab2k@gmail.com',
-  },
-
-  {
-    img: '/public/logoTech/aws.svg',
-    person: 'Pol Valle',
-    position: 'Frontend dev',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra dui ac ante viverra vestibulum. Suspendisse non orci eget ipsum venenatis egestas ut a odio. Suspendisse finibus placerat lectus vel vulputate. Sed sodales a est et sodales. In hac habitasse platea dictumst. Sed velit neque, imperdiet sed fringilla mattis, consequat nec ligula. Praesent molestie libero nibh, sed sollicitudin dolor faucibus ut.',
-    link: 'dsds',
-    github: 'https://github.com/Doplax',
-    linkdin: 'https://www.linkedin.com/in/pol-valle-montes/',
-    mailto: 'mailto:doplax@gmail.com ',
-  },
-  {
-    img: '/public/logoTech/aws.svg',
-    person: 'Francisco Suárez',
-    position: 'Frontend dev',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque pharetra dui ac ante viverra vestibulum. Suspendisse non orci eget ipsum venenatis egestas ut a odio. Suspendisse finibus placerat lectus vel vulputate. Sed sodales a est et sodales. In hac habitasse platea dictumst. Sed velit neque, imperdiet sed fringilla mattis, consequat nec ligula. Praesent molestie libero nibh, sed sollicitudin dolor faucibus ut.',
-    link: 'dsds',
-    github: 'https://github.com/PaquitoGR',
-    linkdin: 'https://www.linkedin.com/in/francisco-a-suarez/',
-    mailto: '',
-  },
-];
-
+import { team } from '../data/team';
 export const OurTeam = () => {
   return (
     <section className=" w-full  py-10 flex flex-col justify-center mx-auto px-10">
@@ -90,6 +42,11 @@ export const OurTeam = () => {
                           className="stroke-tertiary"
                         />
                       </Link>
+                      <section
+                        dangerouslySetInnerHTML={{
+                          __html: member.readMore,
+                        }}
+                      />
                     </div>
                   </main>
                   <footer className="flex justify-center gap-10">
