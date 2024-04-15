c5d7b00521d937074551b592689dc3ecccd21ebe
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-15 16:53:01 +0200

    fix: production urls

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://www.adoptaunpeludo.com/api',
-        target: 'http://localhost:5000/api',
+        target: 'https://www.adoptaunpeludo.com/api',
+        // target: 'http://localhost:5000/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: true,
