98c6c5c61d98e053e3bb033391191ce89c38b6ca
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-15 13:18:42 +0200

    Refactor: change layout and style for devops section

--- src/pages/Public/AboutUs/components/DevOps.jsx
+++ src/pages/Public/AboutUs/components/DevOps.jsx
@@ -4,24 +4,28 @@ import { technologiesDevops } from '../data/technologies';
 import ModalTechnologies from './ModalTechnologies';
 export const DevOps = () => {
   return (
-    <section className="w-full min-h-80 bg-white flex flex-col justify-center mx-auto px-10">
+    <section className="w-full min-h-80 bg-white flex flex-col justify-center mx-auto px-10 pb-10">
       <section className=" max-w-screen-xl w-full   mx-auto">
         <header className="flex justify-start font-lobster text-5xl  text-primary py-10 ">
           DevOps
         </header>
         <main className="flex justify-start  flex-col gap-5 ">
-          <article className="text-pretty font-poppins leading-6 max-w-screen-md pb-20">
-            En la sección DevOps de adoptaunpeludo.com, hemos aplicado una
-            variedad de tecnologías y prácticas para garantizar un despliegue
-            eficiente y confiable de la aplicación. Utilizamos AWS EC2 para
-            alojar la aplicación y Docker para contenerizarla, lo que nos
-            permite tener un entorno de desarrollo consistente y portátil. Para
-            la implementación continua y la entrega continua (CI/CD),
-            aprovechamos GitHub Actions, lo que automatiza el proceso de
-            construcción, prueba y despliegue de la aplicación con cada cambio
-            de código. Utilizamos NGINX como un proxy inverso para servir todos
-            los servicios del backend y el frontend, mejorando la eficiencia y
-            la seguridad de la aplicación.
+          <article className="text-pretty font-poppins leading-6 max-w-screen-md ">
+            <p className="pb-5">
+              En la sección DevOps de adoptaunpeludo.com, hemos aplicado una
+              variedad de tecnologías y prácticas para garantizar un despliegue
+              eficiente y confiable de la aplicación. Utilizamos AWS EC2 para
+              alojar la aplicación y Docker para contenerizarla, lo que nos
+              permite tener un entorno de desarrollo consistente y portátil.
+            </p>
+            <p className="pb-5">
+              Para la implementación continua y la entrega continua (CI/CD),
+              aprovechamos GitHub Actions, lo que automatiza el proceso de
+              construcción, prueba y despliegue de la aplicación con cada cambio
+              de código. Utilizamos NGINX como un proxy inverso para servir
+              todos los servicios del backend y el frontend, mejorando la
+              eficiencia y la seguridad de la aplicación.
+            </p>
           </article>
           <header className="text-start font-lobster text-3xl  text-secondary  capitalize">
             tecnologías devOps
@@ -49,6 +53,7 @@ export const DevOps = () => {
                         __html: technology.description,
                       }}
                     />
+                    <span>...</span>
                   </div>
                   <div className="flex justify-end mr-16 ">
                     <ModalTechnologies technology={technology} />
