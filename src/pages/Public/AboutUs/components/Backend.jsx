51d5521a2add1927a0e3325311475fbb6cadf8e1
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-15 02:06:32 +0200

    feat: backend section complete with techs

--- src/pages/Public/AboutUs/components/Backend.jsx
+++ src/pages/Public/AboutUs/components/Backend.jsx
@@ -1,36 +1,69 @@
-import { Button, Link } from '@nextui-org/react';
+import { Image } from '@nextui-org/react';
+import { technologiesBackend } from '../data/technologies';
+import { H2Title } from '../../../../components';
+import ModalTechnologies from './ModalTechnologies';
 
 export const Backend = () => {
   return (
     <section className="w-full min-h-80 bg-black flex flex-col justify-center px-10">
       <section className=" max-w-screen-xl w-full   mx-auto">
         <header className="flex justify-end font-lobster text-5xl  text-primary py-10 ">
-          Arquitectura Back-end
+          Back-end
         </header>
-        <main className=" flex justify-end gap-5 w-full">
-          <article className="text-pretty font-poppins leading-6 text-white max-w-screen-md text-right">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            pellentesque tempus lacinia. Sed mattis in magna non malesuada.
-            Suspendisse eu nisl sodales purus malesuada lacinia. Aliquam nec
-            odio a justo pellentesque semper vitae molestie ipsum. Ut semper
-            commodo maximus. Duis egestas sed justo eget molestie. Vestibulum
-            ante ipsum primis in faucibus orci luctus et ultrices posuere
-            cubilia curae; Mauris dui augue, finibus et nulla a, ornare congue
-            ex. Aenean magna mauris, consequat ac nisl ut, ornare varius mauris.
-            Fusce dui diam, maximus eget vestibulum vitae, cursus eu leo.
-            Curabitur aliquet dapibus lacus lobortis pharetra. Pellentesque
-            lacus massa, varius nec pretium nec, sagittis id massa. Nam id
-            ultrices purus, ullamcorper pellentesque dui. Pellentesque ligula
-            lorem, auctor consectetur justo id, semper accumsan dolor.
+        <main className=" flex justify-end gap-5 flex-col">
+          <article className="text-pretty font-poppins leading-6 text-white max-w-screen-md self-end text-right">
+            El backend de adoptaunpeludo.com es una arquitectura robusta y
+            escalable, diseñada para manejar todas las funcionalidades clave de
+            la aplicación. Basado en microservicios, cada componente tiene una
+            responsabilidad específica y se comunica entre sí de manera
+            eficiente utilizando RabbitMQ. La API, desarrollada en Node.js y
+            utilizando una base de datos relacional PostgreSQL con Prisma como
+            ORM, gestiona todas las solicitudes relacionadas con la
+            autenticación, el CRUD de usuarios, animales y los chats. Además,
+            cuenta con un servicio de envío de correos electrónicos para
+            notificaciones, un servidor de websockets para comunicaciones en
+            tiempo real, un asistente de IA implementado con Express y
+            LangChain, y un servicio de monitorización para garantizar la
+            estabilidad del sistema. Este backend está completamente dockerizado
+            y desplegado en AWS EC2, con un sólido flujo de integración y
+            entrega continua (CICD) para garantizar despliegues sin problemas.
+          </article>
+          <header className="text-right font-lobster text-3xl  text-secondary  capitalize">
+            tecnologías backend
+          </header>
+          <article className="flex flex-wrap justify-center xl:justify-end max-w-screen-xl mx-auto">
+            {technologiesBackend.map((technology, index) => {
+              return (
+                <article
+                  key={index}
+                  className="flex flex-col w-96 p-5 text-white self-end"
+                >
+                  <div className="flex items-center gap-3 pb-5 self-end ">
+                    <Image
+                      src={technology.img}
+                      width={'50px'}
+                      alt={technology.title}
+                    />
+
+                    <H2Title title={technology.title} />
+                  </div>
+                  <div>
+                    <section
+                      className="text-pretty truncate h-20 leading-5  "
+                      dangerouslySetInnerHTML={{
+                        __html: technology.description,
+                      }}
+                    />
+                  </div>
+                  <div className="flex justify-end mr-16 ">
+                    <ModalTechnologies technology={technology} />
+                  </div>
+                </article>
+              );
+            })}
           </article>
         </main>
-        <footer className="py-10 flex justify-end">
-          <Link href="http://www.google.es" target="_blank">
-            <Button className="font-lobster text-3xl bg-primary text-white py-6 px-5 capitalize">
-              Ver diagrama
-            </Button>
-          </Link>
-        </footer>
+        <footer className="py-10 flex justify-end"></footer>
       </section>
     </section>
   );
