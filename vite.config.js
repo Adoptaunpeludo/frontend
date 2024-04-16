ad3d5a434819c716e2cbe0bb64edec0e2fa46854
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-04-17 00:13:10 +0200

    refactor: move all config variables to .env file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
 import svgr from 'vite-plugin-svgr';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://www.adoptaunpeludo.com/api',
-        // target: 'http://localhost:5000/api',
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: true,
