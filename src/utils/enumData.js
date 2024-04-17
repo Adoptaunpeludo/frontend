e1f42d7f3c3b2f3f8240f40461d576f0f3d2e17e
Juan Manuel Acosta Benitez <jmab2k@gmail.com>
2024-04-17 19:20:05 +0200

    Fix: colors status

--- src/utils/enumData.js
+++ src/utils/enumData.js
@@ -176,17 +176,17 @@ export const ageRanges = [
 
 export const statusColorMap = {
   fostered: 'success',
-  'en acogida': 'success',
+  'En Acogida': 'success',
   awaiting_home: 'success',
-  'esperando un hogar': 'success',
+  'Esperando un hogar': 'success',
   published: 'success',
   publicado: 'success',
-  reservado: 'warning',
+  Reservado: 'warning',
   reserved: 'warning',
   pending: 'warning',
   pendiente: 'warning',
   adopted: 'danger',
-  adoptado: 'danger',
+  Adoptado: 'danger',
   rejected: 'danger',
   rechazado: 'danger',
 };
